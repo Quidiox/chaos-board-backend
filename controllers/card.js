b84a74c6a4e988ff8fa0a66e46d602d382e68c52
Quidiox <33523865+Quidiox@users.noreply.github.com>
2018-05-12 13:50:51 +0300

    card delete

--- controllers/card.js
+++ controllers/card.js
@@ -42,11 +42,16 @@ cardRouter.post('/:containerId', async (req, res) => {
 
 cardRouter.delete('/:containerId/:cardId', async (req, res) => {
   try {
-    await Card.findByIdAndRemove(req.params.cardId)
-    await Container.update(
-      { _id: req.params.containerId },
-      { $pull: { cards: req.params.cardId } }
-    )
+    const card = await Card.findByIdAndRemove(req.params.cardId)
+    const container = await Container.findById(req.params.containerId).populate('cards')
+    const cards = container.cards.filter(card => card.id !== req.params.cardId).map(c => {
+      if(c.position > card.position) {
+        c.position -= 1
+      }
+      return c
+    })
+    container.cards = cards
+    await container.save()
     res.status(204).end()
   } catch (error) {
     console.log(error)
