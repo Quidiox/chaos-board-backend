1a8a4b6c35819b980a2a8c278992b7b9b7572026
Quidiox <33523865+Quidiox@users.noreply.github.com>
2018-07-16 13:28:11 +0300

    change board members

--- controllers/board.js
+++ controllers/board.js
@@ -3,6 +3,16 @@ const Board = require('../models/board')
 const Container = require('../models/container')
 const Card = require('../models/card')
 
+boardRouter.get('/:boardId/members', async (req, res) => {
+  try {
+    const board = await Board.findById(req.params.boardId)
+    res.json(board)
+  } catch (error) {
+    console.log(error)
+    res.status(400).json({ error: 'failed to get board'})
+  }
+})
+
 boardRouter.get('/:boardId', async (req, res) => {
   try {
     const board = await Board.findById(req.params.boardId).populate({
@@ -36,26 +46,13 @@ boardRouter.put('/:boardId/addmember', async (req, res) => {
   try {
     const { boardId, members } = req.body
     const board = await Board.findById(boardId)
-    const combined = [...new Set([...members, ...board.members])]
+    const combined = [...new Set([req.user.id, ...members])]
     board.members = combined
     const savedBoard = await board.save()
     res.json(savedBoard)
   } catch (error) {
     console.log(error)
     res.status(400).json({ error: 'adding member to board failed'})
-  }
-})
-
-boardRouter.put('/:boardId/removemember', async (req, res) => {
-  try {
-    const {boardId, members} = req.body
-    const board = await Board.findById(req.params.boardId)
-    board.members = board.members.filter(id => id !== req.params.boardId)
-    const savedBoard = await board.save()
-    res.json(savedBoard)
-  } catch (error) {
-    console.log(error)
-    res.status(400).json({ error: 'removing member from board failed'})
   }
 })
 
